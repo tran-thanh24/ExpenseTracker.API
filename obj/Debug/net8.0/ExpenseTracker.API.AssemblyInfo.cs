ab4b74dd4f6027af64b6cd166b92603293a6e71c
tran-thanh24 <148622471+tran-thanh24@users.noreply.github.com>
2026-03-25 23:43:06 +0700

    connect api with docker

--- obj/Debug/net8.0/ExpenseTracker.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExpenseTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+447a17080b7e309f7d91e0b10205e9296d8248c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7026233741eac9678286243240dc087792087818")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
